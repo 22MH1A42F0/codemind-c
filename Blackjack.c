9b24e565b9c67a78754335a56aa283a1718c14fd
22MH1A42F0 <119723543+22MH1A42F0@users.noreply.github.com>
2022-12-21 18:30:06 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     c=21-a-b;     if(a+b+c==21&&c>=1&&c<=10)     {         printf("%d",c);     }     else     {         printf("-1");     } }

---
+++ Blackjack.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=21-a-b;
+    if(a+b+c==21&&c>=1&&c<=10)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
