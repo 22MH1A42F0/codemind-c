1fc7b3da62051931608e528517f5bfc1513e08ad
22MH1A42F0 <119723543+22MH1A42F0@users.noreply.github.com>
2022-12-27 22:24:00 +0530

    #include<stdio.h> int main() {     int n,m,r,l=0;     scanf("%d",&n);     while(n>0)     {         r=n%10;         if(l<r)         {             l=r;         }         n=n/10;     }     printf("%d ",l); }

---
+++ Largest_Digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,r,l=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        if(l<r)
+        {
+            l=r;
+        }
+        n=n/10;
+    }
+    printf("%d
+",l);
+}
