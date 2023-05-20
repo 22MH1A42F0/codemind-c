f90b5d61b20e7a41a4ac4f186a19db353073c895
22MH1A42F0 <119723543+22MH1A42F0@users.noreply.github.com>
2023-05-20 11:36:37 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a>b&&a>c)     {         if(b>c)         {             printf("%d",a+b);         }         else         {             printf("%d",a+c);         }     }     else if(a<b&&b>c)     {         if(a>c)         {             printf("%d",b+a);         }         else         {             printf("%d",b+c);         }     }     else     {         if(b>a)         {             printf("%d",c+b);         }         else         {             printf("%d",c+a);         }     } }

---
+++ Hermoine_and_Spells.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    {
+        if(b>c)
+        {
+            printf("%d",a+b);
+        }
+        else
+        {
+            printf("%d",a+c);
+        }
+    }
+    else if(a<b&&b>c)
+    {
+        if(a>c)
+        {
+            printf("%d",b+a);
+        }
+        else
+        {
+            printf("%d",b+c);
+        }
+    }
+    else
+    {
+        if(b>a)
+        {
+            printf("%d",c+b);
+        }
+        else
+        {
+            printf("%d",c+a);
+        }
+    }
+}
