b9c9f40a6ec6a5973666c9b0874e0537a3bc00d8
22MH1A42F0 <119723543+22MH1A42F0@users.noreply.github.com>
2023-05-20 11:19:41 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {        if(a[i]==0)        {            printf("%d ",a[i]);        }     }      for(i=0;i<n;i++)      {          if(a[i]==1)          {              if(a[i]==1)              {                  printf("%d ",a[i]);              }          }      } }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       if(a[i]==0)
+       {
+           printf("%d ",a[i]);
+       }
+    }
+     for(i=0;i<n;i++)
+     {
+         if(a[i]==1)
+         {
+             if(a[i]==1)
+             {
+                 printf("%d ",a[i]);
+             }
+         }
+     }
+}
